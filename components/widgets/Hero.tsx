18677bb6d8439fc182faf443b416c374f8624b00
Abdellah DAMRI <damriabdellah@gmail.com>
2023-12-02 18:26:32 +0100

    feat: fixin the build issues and favicon

--- components/widgets/Hero.tsx
+++ components/widgets/Hero.tsx
@@ -5,14 +5,14 @@ const Hero = () => {
       className='flex flex-col gap-4 items-start justify-start py-12 md:py-24'
     >
       <h1 className='text-4xl md:text-5xl lg:text-7xl leading-[1.2] font-medium'>
-        "Code. Create. Captivate."
+        &quot;Code. Create. Captivate.&quot;
       </h1>
       <p className='text-[20px] lg:text-2xl leading-[1.6] lg:leading-[40px] tracking-wide'>
         As a junior Full-Stack Developer, I embrace the power of code to create
         captivating digital experiences. With a passion for innovation and a
-        keen eye for detail, I'm on a mission to turn ideas into interactive
-        realities. Join me on this coding adventure where each line is a
-        brushstroke, and every project is a captivating masterpiece in the
+        keen eye for detail, I&apos;m on a mission to turn ideas into
+        interactive realities. Join me on this coding adventure where each line
+        is a brushstroke, and every project is a captivating masterpiece in the
         digital realm.
       </p>
       <h2
